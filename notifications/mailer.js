b55104cd26866f3a19f1982cb5806a24e0f6a73e
codeRISEIO <33420264+coderiseio@users.noreply.github.com>
2018-04-27 02:06:33 -0400

    Update mailer.js

--- notifications/mailer.js
+++ notifications/mailer.js
@@ -4,19 +4,18 @@ var buildDisplayName = process.env.BUILD_DISPLAY_NAME
 var gitBranch        = process.env.GIT_BRANCH
 var gitCommit        = process.env.GIT_COMMIT
 var pipelineId       = process.env.PIPELINE_ID
-var ibmUrl           = "https://console.bluemix.net/devops/pipelines/"
-var senderAddress    = "Equibit Builds <equibit.builds@gmail.com>"
 var recipientAddress = process.env.RECIPIENT_ADDRESS
 var projectName      = process.env.PROJECT_NAME
+var ibmUrl           = "https://console.bluemix.net/devops/pipelines/"
 
 console.log(process.env)
 
 // create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("smtps://equibit.builds%40gmail.com:"+encodeURIComponent('Canada@2018') + "@smtp.gmail.com:465");
+var smtpTransport = nodemailer.createTransport("smtps://" + process.env.EMAIL_ID + ":"+encodeURIComponent('" + process.env.EMAIL_PWD + "') + "@smtp.gmail.com:465");
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: senderAddress, // sender address
+    from: "CI Builds <" + process.env.EMAIL_ID + ">", // sender address
     to: recipientAddress, // list of receivers
     subject: "[" + projectName + "] BUILD " + buildDisplayName + "(" + gitBranch + "-" + gitCommit + ")", // Subject line
     html: "<b>Project:" + projectName + "</b><br /><br /><b>Build " + buildDisplayName + ": PASSED</b><br /><br />LINK: " + ibmUrl + pipelineId + "?env_id=ibm:yp:us-south<br /><br />" // html body
