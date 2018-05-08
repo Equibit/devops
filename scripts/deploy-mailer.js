60f5d4423939897c874098208447902abf6ae76d
Harmeek Jhutty <hjhutty@coderise.io>
2018-05-07 22:24:15 -0400

    Added smtp for emails

--- scripts/deploy-mailer.js
+++ scripts/deploy-mailer.js
@@ -20,8 +20,9 @@ var ibmUrl           = "https://console.bluemix.net/devops/pipelines/"
 
 var smtpTransport = nodemailer.createTransport({
         host: 'smtp.office365.com', // Office 365 server
-        port: 25,     // secure SMTP
+        port: 587,     // secure SMTP
         secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
+        requireTLS: true,
         auth: {
             user: senderAddress,
             pass: senderPassword
