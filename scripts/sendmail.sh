d6606b5306b54671bbd011fd3b31a7109782ddf3
Harmeek Jhutty <hjhutty@coderise.io>
2018-05-07 20:53:33 -0400

    Added email notification scripts

---
+++ scripts/sendmail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
+echo "BUILD STATUS: $BUILD_STATUS"
+echo "Sending email notification to: $RECIPIENT_ADDRESS"
+npm install nodemailer
+node mailer.js
